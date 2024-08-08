87d685c348ee3b1ac146033b2256c5d920d65067
Mohamad Shaheen <mhmd.shahin@outlook.com>
2024-08-08 18:14:40 +0300

    Added configuration

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+import json
+import os
 from utils import opentdb_questions_utils
 from utils import trivia_questions_utils
 from utils import quiz_questions_utils
 
 
 def main():
-    opentdb_questions_utils.get_and_store_questions()
-    opentdb_questions_utils.get_and_store_categories()
-    trivia_questions_utils.get_and_store_questions_and_categories()
-    quiz_questions_utils.get_and_store_questions_and_categories()
+    if not os.path.exists('config/api_request.json'):
+        raise FileNotFoundError('config/api_request.json does not exist')
+    with open('config/api_request.json', 'r') as file:
+        data = json.load(file)
+
+    if data['opentdb_questions'] == 0:
+        opentdb_questions_utils.get_and_store_questions()
+    if data['opentdb_categories'] == 0:
+        opentdb_questions_utils.get_and_store_categories()
+    if data['trivia_questions'] == 0:
+        trivia_questions_utils.get_and_store_questions_and_categories()
+    if data['quiz_questions'] == 0:
+        quiz_questions_utils.get_and_store_questions_and_categories()
+
+    with open('config/api_request.json', 'w') as file:
+        data['opentdb_questions'] = 1
+        data['opentdb_categories'] = 1
+        data['trivia_questions'] = 1
+        data['quiz_questions'] = 1
+        json.dump(data, file, indent=4)
+
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,12 +1,26 @@
+import json
 import logging
+import os.path
 from datetime import datetime
-
 import uvicorn
 from fastapi import FastAPI
 from routes import questions_route
 
 app = FastAPI()
-logging.basicConfig(filename='logs/app.log', level=logging.INFO, force=True)
+if not os.path.exists('config/logs.json'):
+    raise FileNotFoundError('config/logs.json does not exist')
+
+with open('config/logs.json', 'r') as file:
+    data = json.load(file)
+
+logging.basicConfig(filename=f'logs/app{data['app_value']}.log', level=logging.INFO, force=True)
+with open(f'logs/app{data['app_value']}.log', 'r') as file:
+    if len(file.readlines()) >= 500:
+        with open('config/logs.json', 'w') as config_file:
+            data['app_value'] += 1
+            json.dump(data, config_file, indent=4)
+        logging.info(f'A new log file \'logs/app{data['app_value']}.log\' was created due to exceeding 500 lines in the current log file - [{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}]')
+        logging.basicConfig(filename=f'logs/app{data['app_value']}.log', level=logging.INFO, force=True)
 
 @app.get('/')
 async def root():
@@ -16,4 +30,4 @@ async def root():
 app.include_router(questions_route.router, prefix='/questions', tags=['questions'])
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run(app, host='localhost', port=8000)
